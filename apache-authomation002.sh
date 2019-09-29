f678f2ac032d3aaaaa7a80c7ebd3aeec8a27b5f8
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-09-29 02:22:05 -0700

    add code to allow htaccess
    substitutes AllowOverride All in the /var/www/html directory to allow use of .htaccess

--- apache-authomation002.sh
+++ apache-authomation002.sh
@@ -1,23 +1,26 @@
 yum -y install httpd mod_ssl                                                                    # install apache and SSL support
 systemctl start httpd                                                                           # start apache
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                                 # comment out the welcome page
+
 echo "<html><body><h1>Hi there NTI 300</h1><br />
 <a href="page2.html">See if you have permission for page 2</a>
 </body></html>" > /var/www/html/index.html                                                      # create custom welcome page
+
 echo "<html><body><h1>You made it to my page 2</h1></body></html>" > /var/www/html/page2.html   # create custom page2
 
-# touch /var/www/html/.htpasswd                                                                   # create the password file
+# extra credit
+sed -i '151s/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf                  # allow htaccess
 
 echo '                                                                                              
 thom:$apr1$fEy2hp9s$TMQCOlvtGZcoGvwbPEIdL/
-nicole:$apr1$PUSCxuzg$aeA7lWE6hhtDt9T9QrePy0 ' > /var/www/html/.htpasswd                        # create users
-
-# touch > /var/www/html/.htaccess                                                                 # create htaccess file
+nicole:$apr1$PUSCxuzg$aeA7lWE6hhtDt9T9QrePy0 ' > /usr/local/etc/.htpasswd                       # create users
 
 echo '
+<FilesMatch "^page2\.html$">
 AuthType Basic
-AuthName "My Protected Area"
-AuthUserFile /var/www/html/.htpasswd
-Require valid-user' > /var/www/html/.htaccess                                                  # create restrictions
+AuthName "Mypassword"
+AuthUserFile /usr/local/etc/.htpasswd
+Require user thom nicole
+</FilesMatch>' > /var/www/html/.htaccess                                                        # restrict page 2
 
-systemctl restart httpd                                                                        # restart apache 
+systemctl restart httpd                                                                         # restart apache 
