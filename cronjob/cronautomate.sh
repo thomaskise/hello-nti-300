f4e040590cc075d0f8ff2d74afb9b7bf5ccc92ea
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-10-09 00:55:49 -0700

    updates script - not final

--- cronjob/cronautomate.sh
+++ cronjob/cronautomate.sh
@@ -1,41 +1,19 @@
-yum -y install httpd mod_ssl                                                                    # install apache and SSL support
-systemctl start httpd                                                                           # start apache
+yum -y install httpd mod_ssl                                                                               # install apache and SSL support
+yum install wget                                                                                           # install wget
 
-sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                                 # comment out the welcome page
+# add crontab
+wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/cronjob/crontab                     # get the crontab file from github
+mv -f crontab /var/spool/cron/root                                                                         # move crontab to the proper directory
+crontab /var/spool/cron/root                                                                               # install crontab file
 
+# add cronjob script
+wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/cronjob/updating_webpage.sh         # get the updating_webpage.sh file from github
+mv -f updating_webpage.sh /root/updating_webpage.sh                                                        # move the updating_webpage.sh file to the proper directory
 
-
-echo "#!/bin/bash
-time=$(date)
-<html><body><h1>hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/index.html  # create custom welcome page file that cron will run
-
-chmod +x updating_webpage.sh                                                                    # change permissions on file to make executable
-
-echo "# ┌───────────── minute (0 - 59)
-# │ ┌───────────── hour (0 - 23)
-# │ │ ┌───────────── day of the month (1 - 31)
-# │ │ │ ┌───────────── month (1 - 12)
-# │ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday;
-# │ │ │ │ │                                   7 is also Sunday on some systems)
-# │ │ │ │ │
-# │ │ │ │ │
-# * * * * * command to execute
-*/5 * * * * /bin/echo 'I/'m running updating_webpage.sh' | logger                      # enter log every 5 minutes
-*/5 * * * * /root/updating_webpage.sh                                                 # run job every 5 minutes" >
+# update index page
+time=$(date)                                                                                               # set the time varible for use in html statment
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                                            # comment out the welcome page
+echo "<html><body><h1>hi there, it is $time, how are you?</h1></body></html>"  > /var/www/html/index.html  # create custom welcome page file that cron will run
 
 
-systemctl restart httpd                                                                         # restart apache 
-
-
-#   cd ~
-#     2  cd ../../..
-#     3  yum install httpd
-#     4  vim updating_webpage.sh
-#     5  chmod +x updating_webpage.sh
-#     6  ls - l
-#     7  ./updating_webpage.sh
-#     8  vim updating_webpage.sh
-#     9  sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                                 # comment out the welcome page
-#    10  systemctl restart httpd
-#    11  systemctl restart httpd
-#    12  ./updating_webpage.sh
+systemctl start httpd                                                                                      # start apache
