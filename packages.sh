b55be974cd6256b2c0bcd07f6f4bd5e3c66424e4
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-10-10 18:34:07 -0700

    get packages.txt

--- packages.sh
+++ packages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 yum -y install wget
 
+wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/packages.txt
+
 for packages in ($(cat /etc/packages.txt); do 
   yum -y install $packages;
 done
