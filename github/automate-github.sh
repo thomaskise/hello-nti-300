7a30a2e91a8ff28cd17860ddac08187e79a11b3e
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-10-11 00:22:23 -0700

    add github script

---
+++ github/automate-github.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -e /bin/git ]; then                                                   # check to see if httpd has already been installled
+#@if [ ! -e /bin/git ]; then  (not)                                        # check to see if httpd has already been installled
+   exit 0;                                                                 # exit if it has, because the environment is already installed
+fi
+
+yum -y install git;                                                        # install git
+git config --global user.name "Duck9000"                                   # update global user name
+git config --global user.email thomas.harrington@seattlecentral.edu        # update global email address
+md /repositiories/
+git clone https://github.com/thomaskise/hello-nti-300.git  /repositories/  # clone 
+
