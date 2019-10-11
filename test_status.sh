e2725e4b53130ab3734d888d4acc4b5b7a318d48
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-10-10 19:28:05 -0700

    add test_status.sh

---
+++ test_status.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then    # check to see if the var is set
+	exit 0;
+fi
+
+varname=$( systemctl status $1 | grep Active | awk ‘{print $2}’); 
+varname2="inactive"; 
+
+if [$varname == $varname2]; then 
+  echo "Nooooooo! it is off"; 
+fi
