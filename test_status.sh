7df25230fd9824b2ebc11c8675b632558a336b7b
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-10-10 19:38:08 -0700

    update

--- test_status.sh
+++ test_status.sh
@@ -4,8 +4,8 @@ if [ -z "$1" ]; then    # check to see if the var is set
 	exit 0;
 fi
 
-status=$( systemctl status $1 | grep Active | awk '{print $2}'); 
-inactive="inactive"; 
+status=$( systemctl status $1 | grep Active | awk '{print $2}')
+inactive="inactive"
 
 if [ $status == $inactive ]; then 
   echo "Nooooooo! it is off"; 
