64b95d262f4b2807fcec193cd775c960010a83e9
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-10-13 01:18:03 -0700

    fixes dir name to pagecontrols

--- cronjob/automate-cronjob.sh
+++ cronjob/automate-cronjob.sh
@@ -1,7 +1,7 @@
 # automate the cron job
 
 # add cronjob script
-wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/cronjob/updating_webpage.sh         # get updating_webpage.sh file from github
+wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/pagecontrols/updating_webpage.sh         # get updating_webpage.sh file from github
 mv -f updating_webpage.sh /root/updating_webpage.sh                                                        # move updating_webpage.sh file to the proper directory
 chmod +x /root/updating_webpage.sh                                                                               # make script executable
 
