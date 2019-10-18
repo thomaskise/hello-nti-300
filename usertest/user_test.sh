504ee63d971ade6da25ef73800c892bbf8923341
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-10-17 19:49:27 -0700

    update

--- usertest/user_test.sh
+++ usertest/user_test.sh
@@ -17,28 +17,3 @@ else
   echo "Package is installed";
 fi                      # closes the if statment
 
-: '
-yum -y install "$input" mod-ssl                                                          # Install apache and SSL support
-systemctl start httpd                                                                 # start apache
-sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                       # comment out the default welcome page
-echo "<html><body><h1>Hi there NTI 300</h1><body></html>" >/var/www/html/index.html   # create custom welcome page
-systemctl restart httpd                                                               # restart apache so our changes take effect
-
-
-
-status=$(systemctl status $1 | grep Active | awk '{print $2}')
-inactive="inactive"
-
-if [ $status == $inactive ]; then
-   echo "noooooooooo it is off"
-else 
-   echo "My status is $status"
-fi
-
-
-  chmod +x package_install_test.sh
-  ./package_install_test.sh
-  
-  chmod +x package_on_test.sh
-  ./package_on_test.sh
-  '
