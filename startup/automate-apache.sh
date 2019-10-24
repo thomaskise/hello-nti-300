927372bef8b7f9b54409eca5b98a2615d78cd603
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-10-23 17:28:38 -0700

    run /etc/bashrc using source command and move it to automate-apache.sh

--- startup/automate-apache.sh
+++ startup/automate-apache.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
 # comment out this cond statement before restarting the envoiroment if you want to reinstall
-#if [ -e /usr/sbin/httpd ]; then                                                                            # check to see if httpd has already been installled
-#   exit 0;                                                                                                 # exit if it has, because the environment is already installed
+#if [ -e /usr/sbin/httpd ]; then                                                                           # check to see if httpd has already been installled
+#   exit 0;                                                                                                # exit if it has, because the environment is already installed
 #fi
 
 yum -y install httpd mod_ssl                                                                               # install apache and SSL support
+
+# get and run custom.sh to install custom functions and aliases
+if [ -e /etc/profile.d/custom.sh ]; then                                                                   # check to see if custom.sh has already been installled
+   rm -f /etc/profile.d/custom.sh                                                                          # delete the script so that the new one gets installed
+fi
+
+wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/startup/custom.sh                   # get custom.sh script from github
+mv custom.sh  /etc/profile.d/custom.sh                                                                     # move custom.sh to where it will be executed during startup
+chmod +x /etc/profile.d/custom.sh                                                                          # make script executable
+source /etc/bashrc                                                                                         # run /etc/bashrc which executes the script
 
 # set-up cron job
 wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/cronjob/automate-cronjob.sh         # get cronjob script from github
@@ -25,7 +35,7 @@ chmod +x automate-github.sh                                                     
 ./automate-github.sh                                                                                       # run the script
 rm -f automate-github.sh                                                                                   # delete the script
 
-# set up git and clone the repo
+# get packageinstall.sh and run it to set up packages
 wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/package/packageinstall.sh           # get packages script from github
 chmod +x packageinstall.sh                                                                                 # make script executable
 ./packageinstall.sh                                                                                        # run the script
--- startup/automate-startup.sh
+++ startup/automate-startup.sh
@@ -3,17 +3,6 @@
 
 yum -y install wget                                                                                    # install wget - assume 'yes'
 
-# set up custom.sh
-if [ -e /etc/profile.d/custom.sh ]; then                                                               # check to see if custom.sh has already been installled
-   rm -f /etc/profile.d/custom.sh                                                                      # delete the script so that the new one gets installed
-fi
-
-wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/startup/custom.sh               # get custom.sh script from github
-mv custom.sh  /etc/profile.d/custom.sh                                                                 # move custom.sh to where it will be executed during startup
-chmod +x /etc/profile.d/custom.sh                                                                      # make script executable
-/etc/profile.d/custom.sh                                                                               # run the script jic it's missed (won't execute with leading period)
-
-
 wget https://raw.githubusercontent.com/thomaskise/hello-nti-300/master/startup/automate-apache.sh      # get apache script from github
 chmod +x automate-apache.sh                                                                            # make script executable
 ./automate-apache.sh                                                                                   # run the script
