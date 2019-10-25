8c16cd7d14da1ca08cb51fbe3c55a9269ff5ebbc
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-10-24 19:28:23 -0700

    add comments

--- django/django.sh
+++ django/django.sh
@@ -12,6 +12,6 @@ django-admin --version
 django-admin startproject newproject
 cd newproject/
 python manage.py migrate
-python manage.py createsuperuser
-newproject/settings.py
+python manage.py createsuperuser           # <- work around this to add a user
+newproject/settings.py                     # sed search and replace
 python manage.py runserver 0.0.0.0:8000
