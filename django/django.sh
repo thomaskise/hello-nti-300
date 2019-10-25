ae47180947a200d3b7f546c224948eebe3da6c56
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-10-24 19:57:52 -0700

    clean yum

--- django/django.sh
+++ django/django.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+yum clean all 
 yum -y install python-pip
 sudo pip install virtualenv
 pip install --upgrade pip
@@ -15,6 +16,7 @@ python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com', 'pw123456')" | python manage.py shell
 # python manage.py createsuperuser           # <- work around this to add a user (above)
 myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g" newproject/settings.py 
 echo $myip
 # newproject/settings.py                     # sed search and replace (above)
 python manage.py runserver 0.0.0.0:8000
