16654bdab17391bf9ae2a66618b3afb02956708b
Thom Harrington <43864992+thomaskise@users.noreply.github.com>
2019-10-24 19:38:19 -0700

    fix script issues as defined in comments

--- django/django.sh
+++ django/django.sh
@@ -12,6 +12,9 @@ django-admin --version
 django-admin startproject newproject
 cd newproject/
 python manage.py migrate
-python manage.py createsuperuser           # <- work around this to add a user
-newproject/settings.py                     # sed search and replace
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com', 'pw123456')" | python manage.py shell
+# python manage.py createsuperuser           # <- work around this to add a user (above)
+myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+echo $myip
+# newproject/settings.py                     # sed search and replace (above)
 python manage.py runserver 0.0.0.0:8000
