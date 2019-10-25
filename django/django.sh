e63482be70f5b6770cfe2e5bf008943457fc0033
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-10-25 14:13:36 -0700

    adds comments

--- django/django.sh
+++ django/django.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-yum clean all 
-yum -y install python-pip
-pip install virtualenv
-pip install --upgrade pip
-mkdir ~/newproject
-cd ~/newproject
-virtualenv newenv
-source newenv/bin/activate
-pip install django
-django-admin --version
-django-admin startproject newproject
-cd newproject/
-python manage.py migrate
+yum clean all                                                                                  # clean out the yum cache
+yum -y install python-pip                                                                      # install python
+pip install virtualenv                                                                         # install the virtual environment service
+pip install --upgrade pip                                                                      # upgrade to the lastest version of pip
+mkdir ~/newproject                                                                             # create the project directory
+cd ~/newproject                                                                                # change to the project directory
+virtualenv newenv                                                                              # create the new environment
+source newenv/bin/activate                                                                     # activate the new environement (source newenv/bin/deactivate to kill it)
+pip install django                                                                             # install django framework
+django-admin --version                                                                         # output the version so that it is logged
+django-admin startproject newproject                                                           # start the project
+cd newproject/                                                                                 # change to the project directory
+python manage.py migrate                                                                       # migrate data schema to the environment
+                                                                                               # next line creates the super user as admin 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com', 'pw123456')" | python manage.py shell
-myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
-sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g" newproject/settings.py 
-python manage.py runserver 0.0.0.0:8000
+myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')         # get the ip address
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g" newproject/settings.py         # assign the ip address to the project settings
+python manage.py runserver 0.0.0.0:8000                                                        # run da server
