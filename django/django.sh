ba67896721f2154e8dc54d6a1a260afa4fec3252
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-10-25 13:37:02 -0700

    removes extraneous comments and blank lines

--- django/django.sh
+++ django/django.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 yum clean all 
 yum -y install python-pip
-sudo pip install virtualenv
+pip install virtualenv
 pip install --upgrade pip
 mkdir ~/newproject
 cd ~/newproject
@@ -14,9 +13,6 @@ django-admin startproject newproject
 cd newproject/
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com', 'pw123456')" | python manage.py shell
-# python manage.py createsuperuser           # <- work around this to add a user (above)
 myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
 sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g" newproject/settings.py 
-echo $myip
-# newproject/settings.py                     # sed search and replace (above)
 python manage.py runserver 0.0.0.0:8000
