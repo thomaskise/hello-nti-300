cd52e15c6c9a201358025cbeecf983d8b95a0a02
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-11-18 16:42:22 -0800

    updates startup script name

--- django/newPyInstanceGCP.py
+++ django/newPyInstanceGCP.py
@@ -22,7 +22,7 @@ def list_instances(compute, project, zone):
     return result['items']
     
 def create_instance(compute, project, zone, name):
-    startup_script = open('startup-script.sh', 'r'). read()
+    startup_script = open('mirror_repo.py', 'r'). read()
     image_response = compute.images().getFromFamily(project='centos-cloud', family='centos-7').execute()
     source_disk_image = image_response['selfLink']
     machine_type = "zones/%s/machineTypes/f1-micro" % zone
